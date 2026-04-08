003f6a882b43e40fe1044013d9007664139dcbb5
Madhusudhan Chaudhary <sawuriya007@gmail.com>
2026-04-08 19:26:33 +0545

    project add

--- portfolio/app/models.py
+++ portfolio/app/models.py
@@ -5,8 +5,8 @@ class Projects(models.Model):
     title=models.CharField(max_length=200)
     desc=models.TextField()
     tech=models.CharField(max_length=200)
-    github_link=models.TextField(default=None)
-    live_link=models.TextField(default=None)
+    github_link=models.TextField(null=True,blank=True)
+    live_link=models.TextField(null=True,blank=True)
     
     def __str__(self):
         return self.title
